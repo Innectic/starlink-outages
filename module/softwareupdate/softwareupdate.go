7e8215a346778b3d6e1f14b44d3559ad7e25904e
Innectic <innectic@gmail.com>
2021-07-31 13:33:45 -0700

    feat: start on software update module

---
+++ module/softwareupdate/softwareupdate.go
@@ -0,0 +1,33 @@
+package softwareupdate
+
+import (
+	"github.com/innectic/starlinkoutages/log"
+	"github.com/innectic/starlinkoutages/rpc"
+	"github.com/innectic/starlinkoutages/module"
+)
+
+type SoftwareUpdateModule struct {
+	c chan module.ModuleMessage
+	r rpc.RPCHandler
+}
+
+func NewSoftwareUpdateModule(c chan module.ModuleMessage, r rpc.RPCHandler) SoftwareUpdateModule {
+	return SoftwareUpdateModule{
+		c: c,
+		r: r,
+	}
+}
+
+func (m SoftwareUpdateModule) Init() (module.ModuleDefinition, error) {
+	def := module.ModuleDefinition{
+		Name: "Software Update Monitor",
+		Description: "Monitors the software version of Dishy for updates",
+		Frequency: 1 * time.Minute,
+	}
+
+	return def, nil
+}
+
+func (m SoftwareUpdateModule) Run() error {
+	return nil
+}
